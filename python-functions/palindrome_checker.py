327abdb2c3bc23cca3087931205fd095de1bf83a
Olukayode <theolukayodeasemudara@gmail.com>
2026-05-10 13:09:47 +0100

    palindrome test file still not working fully

--- python-functions/palindrome_checker.py
+++ python-functions/palindrome_checker.py
@@ -1,4 +1,20 @@
+# collect number from user from the terminal
+# define a function that reverses your number
+# define another function that checks whethe the number you provided is a plindrome
+number = int(input("enter your number: "))
+def reverse_number(number):
+    reversed_num = 0
+    while(number != 0):
+        digit = number % 10
+        reversed_num = reversed_num * 10 + digit
+        number = number / 10
+    return reversed_num
+
 def is_palindrome(number):
-    return 4321
+    reversed = reverse_number(number)
+    return number == reversed
     
-is_palindrome(1234)
+if is_palindrome:
+    print("number is a palindrome")    
+    
+print(is_palindrome(number))
--- python-functions/test_palindrome_checker.py
+++ python-functions/test_palindrome_checker.py
@@ -7,7 +7,17 @@ class Palindrome_Palindrome_App(TestCase):
     def test_that_function_actually_exists(self):
         self.assertTrue(callable(is_palindrome))
         
+    def test_that_function_actually_exists(self):
+        self.assertTrue(callable(reverse_number))
+        
+    def test_that_function_reverses_number(self):
+        actual = reverse_number(1234)
+        expected = 4321
+        self.assertEqual(actual, expected)
+           
+            
     def test_that_number_user_enters_is_palindrome(self):
         actual = is_palindrome(1234)
         expected = 4321
         self.assertEqual(actual, expected)
+        
