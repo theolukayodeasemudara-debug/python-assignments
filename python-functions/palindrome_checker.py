fc7cf29d9cfc9f1d51d5a013d4b30b87d16a6cfb
Olukayode <theolukayodeasemudara@gmail.com>
2026-05-10 21:35:12 +0100

    ...

--- python-functions/palindrome_checker.py
+++ python-functions/palindrome_checker.py
@@ -1,30 +1,34 @@
 # collect number from user from the terminal
 # define a function that reverses your number
 # define another function that checks whether the number you provided is a plindrome
-# define a number that checks whether your number is a prime number
+# define a method that checks whether your number is a prime number
+# display is a palindrome when the requirement/condition is met
+# display whether it is a prime number if the conditions is met
 
 number = int(input("enter your number: "))
 def is_palindrome(value):
     reversed_num = 0
-    while value != 0:
+    while value > 0:
         digit = value % 10 
         reversed_num = reversed_num * 10 + digit
-        value = value/10
+        value = value//10
     return reversed_num
 
-def is_prime_number(number):
-    return ...
+def is_prime_number(value):
+    if value < 2:
+        return False
+    if value % value == 1:
+        return True
+    else:
+        return False
+    return True
     
 if is_palindrome(number):
     print(f"{number} is a palindrome")
 else:
      print(f"{number} is not a palindrome")
      
-     
-        # int reversedNum = 0;
-        # while(number != 0){
-        #     int digit = number % 10;
-        #     reversedNum = reversedNum * 10 + digit;
-        #     number = number/10;
-        # }
-        # return reversedNum;
+if is_prime_number(number):
+    print(f"{number} is a prime number")
+else:
+    print(f"{number} is not a prime number")
--- python-functions/test_palindrome_checker.py
+++ python-functions/test_palindrome_checker.py
@@ -1,25 +1,19 @@
 from unittest import TestCase
-
 from palindrome_checker import *
 
 class Palindrome_Palindrome_App(TestCase):
     
     def test_that_function_actually_exists(self):
         self.assertTrue(callable(is_palindrome))
-        self.assertTrue(callable(reverse_number))
+        self.assertTrue(callable(is_prime_number))
         
     def test_that_function_reverses_number(self):
-        actual = reverse_number(1234)
+        actual = is_palindrome(1234)
         expected = 4321
         self.assertEqual(actual, expected)
            
     def test_that_number_user_enters_is_palindrome(self):
-        actual = is_palindrome(1234)
-        expected = 4321
-        self.assertEqual(actual, expected)
+        self.assertTrue(is_palindrome(121))
         
     def test_that_number_is_prime_number(self):
-        actual = is_prime_number(3)
-        expected = 3
-        self.assertEqual(actual, expected)
-        
+        self.assertTrue(is_prime_number(3))
